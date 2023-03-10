d455803c0cbabf48bcea0c0aa7ce7f04b97bff2f
Bliss Jaspis <jaspis_zzt@ymail.com>
2023-03-10 22:05:02 +0700

    make unparse time tobe bad request

--- src/pages/api/chrono.json.ts
+++ src/pages/api/chrono.json.ts
@@ -1,22 +1,27 @@
-import type { APIRoute } from "astro";
-import * as chrono from "chrono-node";
+import type { APIRoute } from "astro"
+import * as chrono from "chrono-node"
 
 export const post: APIRoute = async ({ request }) => {
-  const body = await request.formData();
+    const body = await request.formData()
 
-  if (body.has("time")) {
-    const time = body.get("time") ?? "now";
-    const text = chrono.parse(time.toString());
-    const result = chrono.parseDate(text[0].text);
+    if (body.has("time")) {
+        const time = body.get("time") ?? "now"
+        const text = chrono.parse(time.toString())
+        const result = chrono.parseDate(text[0].text)
 
-    return {
-      body: JSON.stringify({
-        message: "Success loaded time",
-        text_original: text[0].text,
-        result: result,
-      }),
-    };
-  }
+        try {
+            return {
+                body: JSON.stringify({
+                    message: "Success loaded time",
+                    text_original: text[0].text,
+                    result: result,
+                }),
+            }
+        } catch (error) {
+            console.log(error);
+        }
 
-  return new Response(null, { status: 400 });
-};
+    }
+
+    return new Response(null, { status: 400 })
+}
