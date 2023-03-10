f401cc93e9ab3b82f881caaad87514f04b6712e2
Bliss Jaspis <jaspis_zzt@ymail.com>
2023-03-10 14:26:35 +0700

    initialize

---
+++ README.md
@@ -0,0 +1,12 @@
+# ScriptApi
+
+This is all about javascript package collections.
+
+Sometimes it's hard to get the proper package from other lang, espescially if the package or the community not mature enough.
+Start from this desire i make this ScriptAPI.
+
+From this on you can access using API to get all the functions from the package.
+
+## Supported Package
+
+[Chrono](https://github.com/wanasit/chrono)
---
+++ src/pages/api/chrono.json.ts
@@ -0,0 +1,22 @@
+import type { APIRoute } from "astro";
+import * as chrono from "chrono-node";
+
+export const post: APIRoute = async ({ request }) => {
+  const body = await request.formData();
+
+  if (body.has("time")) {
+    const time = body.get("time") ?? "now";
+    const text = chrono.parse(time.toString());
+    const result = chrono.parseDate(text[0].text);
+
+    return {
+      body: JSON.stringify({
+        message: "Success loaded time",
+        text_original: text[0].text,
+        result: result,
+      }),
+    };
+  }
+
+  return new Response(null, { status: 400 });
+};
